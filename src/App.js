def323902e10eb0112554417cb697d164e8fe81d
matthewpieri <mattpieri1@gmail.com>
2020-07-17 22:07:21 -0400

    edit create fields

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Create from './Create';
+import Create  from './layouts/Create';
 import { Game } from './Game';
 import { Search } from './Search';
 import { NavigationBar } from './components/NavigationBar';
 import ViewTeam from './ViewTeam'
+import SimpleTextEdit from './layouts/SimpleTextEdit.js';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           <Route exact path="/search" component={ViewTeam} />
           <Route exact path="/game" component={ViewTeam} />
           <Route exact path="/create" component={Create} />
+          <Route exact path="/edit" component={SimpleTextEdit} />
           <Route component={ViewTeam}/>
           </Switch>
         </Router>
